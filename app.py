52dda9208b6fb1f2f2692deb613a834a16a6814e
Saara <saara.lonnroth@helsinki.fi>
2025-04-08 12:08:20 +0300

    Add not found handling

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def find_item():
 @app.route("/item/<int:item_id>")
 def show_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     return render_template("show_item.html", item=item)
 
 @app.route("/new_item")
@@ -48,6 +50,8 @@ def create_item():
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
     return render_template("edit_item.html", item=item)
@@ -56,6 +60,8 @@ def edit_item(item_id):
 def update_item():
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
+    if not item:
+        abort(404)
     if item["user_id"] != session["user_id"]:
         abort(403)
 
